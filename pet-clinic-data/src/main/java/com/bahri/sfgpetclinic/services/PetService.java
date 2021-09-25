67ae5638b180569c6aae34125c43c57dff85b641
Bahri <raed.bahri@esprit.tn>
2021-09-25 23:44:59 +0200

    Create Interfaces for Pet Clinic Services

---
+++ pet-clinic-data/src/main/java/com/bahri/sfgpetclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package com.bahri.sfgpetclinic.services;
+
+import com.bahri.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/bahri/sfgpetclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package com.bahri.sfgpetclinic.services;
+
+import com.bahri.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
