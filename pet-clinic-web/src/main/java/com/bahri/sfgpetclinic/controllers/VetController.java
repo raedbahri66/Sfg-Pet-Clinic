5826ecd86c52cbcb9507d390e99130750320ce0b
Bahri <raed.bahri@esprit.tn>
2021-09-27 23:47:28 +0200

    Added Vets List and updated controller

--- pet-clinic-web/src/main/java/com/bahri/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/bahri/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package com.bahri.sfgpetclinic.controllers;
 
+import com.bahri.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"/vets"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
